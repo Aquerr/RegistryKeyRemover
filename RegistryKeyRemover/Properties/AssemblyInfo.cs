fa9ac97ae2dd366724716113f2308eb7383bb200
Bartłomiej Stępień <bartlomiej.stepien@folksam.se>
2019-06-10 09:22:26 +0200

    Update Assembly name

--- RegistryKeyRemover/Properties/AssemblyInfo.cs
+++ RegistryKeyRemover/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IntellijRegistry")]
+[assembly: AssemblyTitle("RegistryKeyRemover")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IntellijRegistry")]
+[assembly: AssemblyProduct("RegistryKeyRemover")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
